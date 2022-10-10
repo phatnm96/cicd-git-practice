785af93dc1feb786ac987e6ee6ec13b5e2f5accc
phat nguyen <phatnm96@gmail.com>
2022-10-10 17:25:21 +0700

    add subtract function

--- main.go
+++ main.go
@@ -9,10 +9,21 @@ func sum(a, b int) int {
 	return sum
 }
 
+func subtract (a , b int) int {
+	if a >= b {
+		return a - b
+	} else {
+		return b - a
+	}
+
+}
+
 
 func main() {
 	total := sum(2, 2)
+	subtract := subtract(3,1)
 	fmt.Println("Hello world!")
 	fmt.Println(total)
+	fmt.Println(subtract)
 
 }
